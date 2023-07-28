4f269c934de2e992411ffc2bea7ff1279352ed3c
Jacob Gunther <jacobgunther03+github@gmail.com>
2023-07-28 17:50:26 -0500

    Add JSON tags to query structs

--- response/query.go
+++ response/query.go
@@ -3,16 +3,16 @@ package response
 import "github.com/mcstatus-io/mcutil/description"
 
 type BasicQuery struct {
-	MOTD          description.Formatting
-	GameType      string
-	Map           string
-	OnlinePlayers uint64
-	MaxPlayers    uint64
-	HostPort      uint16
-	HostIP        string
+	MOTD          description.Formatting `json:"motd"`
+	GameType      string                 `json:"game_type"`
+	Map           string                 `json:"map"`
+	OnlinePlayers uint64                 `json:"online_players"`
+	MaxPlayers    uint64                 `json:"max_players"`
+	HostPort      uint16                 `json:"host_port"`
+	HostIP        string                 `json:"host_ip"`
 }
 
 type FullQuery struct {
-	Data    map[string]string
-	Players []string
+	Data    map[string]string `json:"data"`
+	Players []string          `json:"players"`
 }
